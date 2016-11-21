a9b9b6f25a94ea56829bbd1fc636b6f1028d52f1
Lindsey <lewhitley@gmail.com>
2016-11-21 10:58:04 -0800

    add new endpoints for requirements and requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?, :require_same_user
+  helper_method :current_user
 
   def current_user
     @current_user ||= User.find_by(session_token: session[:session_token])
@@ -15,12 +15,12 @@ class ApplicationController < ActionController::Base
   end
 
   def logout
-    current_user.reset_session_token
+    current_user.reset_token!
     session[:session_token] = nil
     @current_user = nil
   end
 
-  def logged_in?
-    !!current_user
+  def require_logged_in
+    render json: {base: ['invalid credentials']}, status: 401 if !current_user
   end
 end
--- docs/api-endpoints.md
+++ docs/api-endpoints.md
@@ -30,3 +30,15 @@
 
 - `POST /api/companies`
 - `GET /api/companies/:id`
+
+### Requirements
+
+- `POST /api/requirements`
+- `DELETE /api/requirements`
+- `GET /api/requirements`
+
+### Requests
+
+- `POST /api/requests`
+- `DELETE /api/requests`
+- `GET /api/requests`

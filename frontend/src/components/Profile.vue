c9f8f2bc18908b14a4932a0c2b7675fe9478ad5c
Amanda Fielding <amandafielding@Amandas-MacBook-Pro.local>
2016-11-27 15:25:45 -0800

    add vue CLI

---
+++ frontend/src/components/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    hey {{ message }}
+  </div>
+</template>
+
+<script>
+const data = {message: 'this is profile'}
+export default {
+  data () {
+    return data
+  }
+}
+</script>
+
+<style>
+</style>

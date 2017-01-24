d22e1e19f00e556d3c0fa4189ab41a0f4b4a6845
Lindsey Whitley <lewhitley@gmail.com>
2017-01-24 15:39:04 -0800

    begin changes to EventManage to allow event update

--- frontend/src/components/EventManage.vue
+++ frontend/src/components/EventManage.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="event-manage">
-    <h2>{{ event.title }}</h2>
-    <span class="manage-type">Event type: {{ event.event_type }}</span>
-    <li>{{ event.city }}, {{ event.state }}</li>
-    <li>{{ calculateDate(event.start_date) }} - {{ calculateDate(event.end_date) }}</li>
-    <li>${{ event.pay }} per {{ event.pay_freq }}</li>
-    <div class="manage-description">{{ event.description }}</div>
-    <ul class="event-requirement-list">
-        <li  class="event-requirement" v-for='requirement in requirements'>{{ requirement.title }}</li>
-    </ul>
-    <div class="manage-company">{{ showAdmin(event.admin_name,event.company_name) }}
-      <div class=""><a :href="event.company_website">Company Website</a></div>
-    </div>
+    <span>Event title <input v-model="event.title" placeholder="title"></span>
+    <span>Event type <input v-model="event.event_type" placeholder="type"></span>
+    <span>Location <input v-model="event.city" placeholder="city">  <input v-model="event.state" placeholder="state"></span>
+    <span>Beginning Date <input type="date" v-model="event.start_date"></span>
+    <span>Ending Date <input type="date" v-model="event.end_date"></span>
+    <span>$<input v-model="event.pay" placeholder="pay rate"> per <input v-model="event.pay_freq" placeholder="pay frequency"></span>
+    <textarea v-model="event.description" placeholder="description"/>
+    <!-- <div>
+      Requirements:
+      <br/>
+      <input type="checkbox" v-model="events.requirements" value="1">no visible tattoos
+      <br/>
+      <input type="checkbox" v-model="events.requirements" value="2">valid driver's license
+      <br/>
+      <input type="checkbox" v-model="events.requirements" value="3">valid TIPS certification
+      <br/>
+      <input type="checkbox" v-model="events.requirements" value="4">no felonies
+    </div> -->
+    <span>Company Website <input v-model="event.company_website" placeholder="website url"></span>
+    <button @click="submitEventChanges(this.$route.params.eventId)">Update Event</button>
     <button @click="navToRequestsManage">View {{ event.applicant_count }} Request(s)</button>
   </div>
 </template>
@@ -33,6 +41,16 @@ export default {
       $.ajax({
         method: 'GET',
         url: '/api/events/' + eventId,
+        success: event => {
+          this.$store.dispatch('getEvent', event)
+        }
+      })
+    },
+    submitEventChanges (eventId) {
+      $.ajax({
+        method: 'POST',
+        url: '/api/events/' + eventId,
+        data: {event: this.$store.state.event},
         success: event => {
           this.$store.dispatch('getEvent', event)
         }
@@ -85,6 +103,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.event-manage {
+  position: absolute;
+  top: 75px;
+}
 
+.event-manage span {
+  display: block;
+  margin-bottom: 10px;
+}
 </style>
